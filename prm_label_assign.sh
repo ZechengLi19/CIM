fe7fec1679f158dc609cc267f806a5618f21d3eb
Mac-lzc <1580773907@qq.com>
2023-04-29 18:10:58 +0800

    Supplementary readme

--- prm_label_assign.sh
+++
@@ -1,5 +0,0 @@
-echo $(nvidia-smi)
-source activate wsis_py_3.6
-srun python ./tools/prm_tools/prm_labelassign.py
-
-echo "Done"
--- train_voc_vgg.sh
+++
@@ -1,7 +0,0 @@
-echo $(nvidia-smi)
-source activate wsis_py_3.6
-
-CUDA_VISIBLE_DEVICES=0 python -u ./tools/train_net_step.py --dataset voc2012trainaug \
---cfg ./configs/baseline/resnet50_cobsbd_refine_mode_mask_fuse.yaml \
-
-echo "Done"
